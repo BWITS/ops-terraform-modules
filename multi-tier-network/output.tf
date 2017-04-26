1452a4891d3c8a0d743f1545ebc00d23f3f2124e
Paul Borg <paul.borg@myob.com>
2017-04-26 17:28:22 +1000

    Fix outputs

--- multi-tier-network/output.tf
+++ multi-tier-network/output.tf
@@ -6,18 +6,14 @@ output "internet_gateway_id" {
   value = "${aws_internet_gateway.main.id}"
 }
 
-output "app_1_subnet_ids" {
-  value = ["${aws_subnet.app_1.*.id}"]
-}
-
-output "app_2_subnet_ids" {
-  value = ["${aws_subnet.app_2.*.id}"]
+output "app_subnet_ids" {
+  value = ["${aws_subnet.app.*.id}"]
 }
 
 output "public_subnet_ids" {
   value = ["${aws_subnet.public.*.id}"]
 }
 
-output "db_subnet_ids" {
-  value = ["${aws_subnet.db.*.id}"]
+output "data_subnet_ids" {
+  value = ["${aws_subnet.data.*.id}"]
 }
